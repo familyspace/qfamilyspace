cdc47e51dab9136c7c71b1d36d8895368fda0ac5
DoctorChe <duncan.reg@yandex.ru>
2019-03-28 17:31:26 +0300

    переделал на аутентификацию через api по логину и паролю (пароль пока только копированием закодированной sha256 последовательности)

--- familyspace/login.py
+++ familyspace/login.py
@@ -1,6 +1,10 @@
+import hashlib
+import json
+
 import requests
 # import urllib2
 # from requests.auth import HTTPBasicAuth
+from hash import hash_string
 
 from PyQt5 import QtWidgets
 # from ui.ui_loginwindow import Ui_LoginWindow
@@ -31,10 +35,23 @@ class Login(QtWidgets.QDialog):
         # oauth = OAuth2Session(client=client)
         # token = oauth.fetch_token(token_url='https://provider.com/oauth2/token', client_id=client_id,
         #                           client_secret=client_secret)
-
-
         # response = requests.get('https://api.github.com/user', auth=HTTPBasicAuth(user, password))
-        response = requests.get('https://api.github.com/user', auth=(user, password))
+        # response = requests.get('https://api.github.com/user', auth=(user, password))
+
+        password_sha256 = hash_string(password)
+        print(password_sha256)
+        # password_sha256 = '8D969EEF6ECAD3C29A3A629280E686CF0C3F5D5A86AFF3CA12020C923ADC6C92'
+
+        headers = {'Accept': 'application/json'}
+        payload = {
+            "login": 'user3',
+            # "password": password_sha256,
+            "password": password,
+        }
+        # response = requests.post('http://localhost:8000/apiauth/signin/', headers=headers, data=json.dumps(payload))
+        response = requests.post('http://localhost:8000/apiauth/signin/', headers=headers, json=payload)
+        # print(response.content.decode('utf8'))
+        print(response.json())
         # response = urllib2.urlopen(urllib2.Request('https://api.github.com/user',
         #                                            headers={'Authorization': 'access_token myToken'}))
         # if self.textName.text() == 'foo' and self.textPass.text() == 'bar':
