f1a0c644767e92bd6ed114fca260fcebb5aa275f
Rejmss <42318019+Rejmss@users.noreply.github.com>
2019-04-08 18:20:42 +0300

    Add files via upload
    Альтернативная версия основного окна

---
+++ qfamilyspace/ui/ui_mainwindow_v2.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+import ui_modalLogInWindow
+
+
+class Ui_MainWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.label = QtWidgets.QLabel('<center><b><h2>Здравствуйте, User</h2></b></center>')
+        self.profileButton = QtWidgets.QPushButton('Профиль')
+        self.logOffButton = QtWidgets.QPushButton('Выйти')
+        self.logOffButton.setStyleSheet('QPushButton {background-color: red; font-weight: bold; font-size: 11pt}')
+        self.contactSearchButton = QtWidgets.QPushButton('Поиск контактов')
+        self.groupSearchButton = QtWidgets.QPushButton('Поиск группы')
+        self.groupCreateButton = QtWidgets.QPushButton('Создать группу')
+        self.yourGroupsButton = QtWidgets.QPushButton('Ваши группы')
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox.addWidget(self.label)
+        self.vbox.addWidget(self.profileButton)
+        self.vbox.addWidget(self.contactSearchButton)
+        self.vbox.addWidget(self.groupSearchButton)
+        self.vbox.addWidget(self.groupCreateButton)
+        self.vbox.addWidget(self.yourGroupsButton)
+        self.vbox.addWidget(self.logOffButton)
+        self.setLayout(self.vbox)
