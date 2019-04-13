a3c05950869cded8251410452b9c0eb3d917fd73
DoctorChe <duncan.reg@yandex.ru>
2019-04-13 17:17:43 +0300

    почищен модуль аутентификации

--- qfamilyspace/ui/dialogs/auth.py
+++ qfamilyspace/ui/dialogs/auth.py
@@ -1,10 +1,8 @@
 import hashlib
 import requests
-# import urllib2
 
 from PyQt5 import QtWidgets, QtCore
 
-# from ui.ui_loginwindow import Ui_LoginWindow
 from qfamilyspace.ui.dialogs.ui_auth import Ui_AuthDialog
 
 
@@ -25,7 +23,6 @@ class Auth(QtWidgets.QDialog):
 
         self.ui = Ui_AuthDialog()
         self.ui.setupUi(self)
-        # self._initUI()
 
         self.ui.lineEditPassword.setEchoMode(QtWidgets.QLineEdit.Password)
         self.ui.lineEditPasswordSignup.setEchoMode(QtWidgets.QLineEdit.Password)
@@ -42,82 +39,6 @@ class Auth(QtWidgets.QDialog):
 
         # Чтение настроек
         self.read_settings()
-
-    # def _initUI(self):
-    #     # self.textName = QtWidgets.QLineEdit(self)
-    #     # self.textPass = QtWidgets.QLineEdit(self)
-    #     # self.buttonLogin = QtWidgets.QPushButton("Login", self)
-    #     # self.buttonLogin.clicked.connect(self.handle_login)
-    #     # self.buttonSignup = QtWidgets.QPushButton("Signup", self)
-    #     # self.buttonSignup.clicked.connect(self.handle_signup)
-    #     # self.checkboxShowPassword = QtWidgets.QCheckBox('Show password', self)
-    #     # self.checkboxSavePassword = QtWidgets.QCheckBox('Save password', self)
-    #     # layout = QtWidgets.QVBoxLayout(self)
-    #     # layout.addWidget(self.textName)
-    #     # layout.addWidget(self.textPass)
-    #     # layout.addWidget(self.checkboxShowPassword)
-    #     # layout.addWidget(self.checkboxSavePassword)
-    #     # layout.addWidget(self.buttonLogin)
-    #     # layout.addWidget(self.buttonSignup)
-    #
-    #     self.layout = QtWidgets.QVBoxLayout(self)
-    #     # Создаем лого
-    #     self.logoLabel = QtWidgets.QLabel('<i><b><h1>Family Space</h1></b></i>')
-    #     self.logoLabel.setAlignment(QtCore.Qt.AlignCenter)
-    #     # Создаем лейблы для логина и пароля
-    #     self.loginLabel = QtWidgets.QLabel('<b><h2>Логин</h2></b>')
-    #     self.loginLabel.setAlignment(QtCore.Qt.AlignCenter)
-    #     self.passLabel = QtWidgets.QLabel('<b><h2>Пароль</h2></b>')
-    #     self.passLabel.setAlignment(QtCore.Qt.AlignCenter)
-    #     # Создаем поля ввода для логина и пароля
-    #     self.loginLine = QtWidgets.QLineEdit()
-    #     self.passLine = QtWidgets.QLineEdit()
-    #     # Добавляем кнопки входа и регистрации
-    #     self.btnLog = QtWidgets.QPushButton('&Войти')
-    #     self.btnLog.setStyleSheet('QPushButton {font-weight: bold; font-size: 11pt;}')
-    #     self.btnReg = QtWidgets.QPushButton('&Зарегистрироваться')
-    #     # Создаем виджет вкладок и сами вкладки
-    #     self.tabs = QtWidgets.QTabWidget()
-    #     self.tabLog = QtWidgets.QWidget()
-    #     self.tabReg = QtWidgets.QWidget()
-    #     # Добавляем вкладки вкладки к виджету
-    #     self.tabs.addTab(self.tabLog, 'Войти')
-    #     self.tabs.addTab(self.tabReg, 'Зарегистрироваться')
-    #     # Генерируем создержимое первой вкладки
-    #     self.tabLog.layout = QtWidgets.QVBoxLayout(self)
-    #     self.tabLog.layout.addWidget(self.logoLabel)
-    #     self.tabLog.layout.addWidget(self.loginLabel)
-    #     self.tabLog.layout.addWidget(self.loginLine)
-    #     self.tabLog.layout.addWidget(self.passLabel)
-    #     self.tabLog.layout.addWidget(self.passLine)
-    #     self.tabLog.layout.addWidget(self.btnLog)
-    #     self.tabLog.setLayout(self.tabLog.layout)
-    #     # Создаем лейблы и поля ввода вкладки регистрации
-    #     self.regLogoLabel = QtWidgets.QLabel('<i><b><h1>Family Space</h1></b></i>')
-    #     self.regLogoLabel.setAlignment(QtCore.Qt.AlignCenter)
-    #     # Добавляем кнопку регистрации
-    #     self.btnReg = QtWidgets.QPushButton('&Зарегистрироваться')
-    #     self.btnReg.setStyleSheet('QPushButton {font-weight: bold; font-size: 11pt;}')
-    #
-    #     # Генерируем содержимое второй вкладки
-    #     self.tabReg.layout = QtWidgets.QVBoxLayout(self)
-    #     self.formBoxLayout = QtWidgets.QFormLayout()
-    #     self.formBoxLayout.addRow(QtWidgets.QLabel('<b><h2>Логин:</h2></b>'), QtWidgets.QLineEdit())
-    #     self.formBoxLayout.addRow(QtWidgets.QLabel('<b><h2>Пароль:</h2></b>'), QtWidgets.QLineEdit())
-    #     self.formBoxLayout.addRow(QtWidgets.QLabel('<b><h2>Повторите пароль:</h2></b>'), QtWidgets.QLineEdit())
-    #     self.formBoxLayout.addRow(QtWidgets.QLabel('<b><h2>Email:</h2></b>'), QtWidgets.QLineEdit())
-    #     self.formBoxLayout.addRow(QtWidgets.QLabel('<b><h2>Телефон:</h2></b>'), QtWidgets.QLineEdit())
-    #     self.tabReg.layout.addWidget(self.regLogoLabel)
-    #     self.tabReg.layout.addLayout(self.formBoxLayout)
-    #     self.tabReg.layout.addWidget(self.btnReg)
-    #     self.tabReg.setLayout(self.tabReg.layout)
-    #
-    #     # Добавляем виджет вкладок к нашему окну
-    #
-    #     self.layout.addWidget(self.tabs)
-    #     self.setLayout(self.layout)
-    #
-    #     self.setWindowModality(QtCore.Qt.ApplicationModal)
 
     def change_pass_status(self):
         self.user["pass_is_sha256"] = False
@@ -170,13 +91,11 @@ class Auth(QtWidgets.QDialog):
 
     def handle_login(self):
 
-        # user = self.textName.text()
         user = self.ui.lineEditLogin.text()
 
         if self.user["pass_is_sha256"]:
             password_sha256 = self.ui.lineEditPassword.text()
         else:
-            # password_sha256 = hash_string(self.textPass.text())
             password_sha256 = hash_string(self.ui.lineEditPassword.text())
 
         token = ""
